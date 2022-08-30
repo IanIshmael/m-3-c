9604f2e3b7ca636899fc1aa46987cb326e7bca39
IanIshmael <107890250+IanIshmael@users.noreply.github.com>
2022-08-30 16:06:00 -0600

    finished js

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,4 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-function generatePassword() {
-  var passwordLength = parseInt(
-    prompt("How long from 8 to 128 characters would you like to choose?")
-  );
-  if (isNaN(passwordLength)) {
-    alert("Only numbers are allowed");
-    return;
-  }
-  if (passwordLength < 8) {
-    alert("Password must be at least 8 characters");
-    return;
-  }
-  if (passwordLength > 128) {
-    alert("Password must be less than 129 characters");
-    return;
-  }
-  
-  var confirmLowercase = confirm(
-    "Would you like your password to include lowercase?"
-  );
-  var confirmUppercase = confirm(
-    "Would you like your password to include uppercase?"
-  );
-  var confirmNumeric = confirm(
-    "Would you like your password to include numbers?"
-  );
-  var confirmSpecial = confirm(
-    "Would you like your password to include special characters?"
-  );
-}
-
-var specialCharacters = [
+var specialCharactersChars = [
   "@",
   "%",
   "+",
@@ -59,10 +25,10 @@ var specialCharacters = [
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numericCharactersChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
+var lowerCasedCharactersChars = [
   "a",
   "b",
   "c",
@@ -92,34 +58,74 @@ var lowerCasedCharacters = [
 ];
 
 // Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
+
+var upperCaseCharactersChars = [];
+
+  for (var i = 0; i < lowerCasedCharactersChars.length; i++) {
+    upperCaseCharactersChars[i] = lowerCasedCharactersChars[i].toUpperCase();
+  }
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+//
+function generatePassword(){
+   var finalPassword = [] 
+  var passwordLength = parseInt(
+    prompt("How long from 8 to 128 characters would you like to choose?")
+  );
+  if (isNaN(passwordLength)) {
+    alert("Only numbers are allowed");
+    return;
+  }
+  if (passwordLength < 8) {
+    alert("Password must be at least 8 characters");
+    return;
+  }
+  if (passwordLength > 128) {
+    alert("Password must be less than 129 characters");
+    return;
+  }
+
+  var lowerCasedCharacters = confirm(
+    "Would you like your password to include lowercase?"
+  );
+  var upperCaseCharacters = confirm(
+    "Would you like your password to include uppercase?"
+  );
+  var numericCharacters  = confirm(
+    "Would you like your password to include numbers?"
+  );
+  var specialCharacters = confirm(
+    "Would you like your password to include special characters?"
+  );
+  if (!lowerCasedCharacters && !upperCaseCharacters && !numericCharacters && !specialCharacters ) {
+    alert("You must choose at least one charater.");
+    return;
+  }
+
+  var allPossibleChars = [];
+  if (lowerCasedCharacters) {
+    allPossibleChars = allPossibleChars.concat(lowerCasedCharactersChars);
+  }
+  if (upperCaseCharacters) {
+    allPossibleChars = allPossibleChars.concat(upperCaseCharactersChars);
+  }
+  if (numericCharacters) {
+    allPossibleChars = allPossibleChars.concat(numericCharactersChars);
+  }
+  if (specialCharacters) {
+    allPossibleChars = allPossibleChars.concat(specialCharactersChars);
+  }
+  
+  for (let i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * allPossibleChars.length);
+    finalPassword.push(allPossibleChars[randomIndex]);
+  }
+  return finalPassword.join("");
+}
+
+
+  
+
 
 
 // Write password to the #password input
--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# m-3-c
+# <Your-Project-Title>
+
+## Description
+
+This project is to show that I wrote JavaScript to help the user make a generated password and also give the user some choices for what they want in their randomized password. 
+
+- What was my motivation? My reasoning for this project was to expierience what it is like to build a website that uses a great amount of Java Script
+- Why did I build this project? 
+- What problem does it solve?
+- What did I learn?
+
+## Table of Contents (Optional)
+
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+
+## Installation
+
+All you really need is any browser. 
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+
+## Features
+
+If your project has a lot of features, list them here.
+
+
+## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+---
+
+
+
+
+
+
